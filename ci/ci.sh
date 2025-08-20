2adec06077d8944c97cca76e2225dabe03b361d0
shenweihai1 <shenweihai1@gmail.com>
2025-08-20 19:51:01 +0000

    Add more ci

--- ci/ci.sh
+++ ci/ci.sh
@@ -3,6 +3,37 @@
 
 set -e  # Exit on error
 
-make -j32 
+# Function 1: Compile
+compile() {
+    make -j32
+}
 
-./build/simpleTransaction
+# Function 2: Run simple transaction test
+run_simple_transaction() {
+    ./build/simpleTransaction 
+}
+
+# Function 3: Run simple Paxos test
+run_simple_paxos() {
+    ./examples/simplePaxos.sh 
+}
+
+# Main entry point with command parsing
+case "${1:-}" in
+    compile)
+        compile
+        ;;
+    simpleTransaction)
+        run_simple_transaction
+        ;;
+    simplePaxos)
+        run_simple_paxos
+        ;;
+    all)
+        # Run all steps in sequence
+        compile
+        run_simple_transaction
+        run_simple_paxos
+        log "All CI steps completed successfully!"
+        ;;
+esac
--- examples/simplePaxos.sh
+++ examples/simplePaxos.sh
@@ -7,5 +7,7 @@ nohup ./build/simplePaxos p2 > a3.log 2>&1 &
 sleep 1
 nohup ./build/simplePaxos learner > a4.log 2>&1 &
 
-tail -f a1.log a2.log a3.log a4.log
+sleep 10 
+
+tail -n 1 a1.log a2.log a3.log a4.log
 
